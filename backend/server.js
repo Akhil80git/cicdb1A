c6839956f5393f090264dd471e32ca154995601b
Akhil80git <akhilgarg8085@gmail.com>
2026-02-18 14:51:52 +0530

    Final test with cicdb1a

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hello DevOps 🚀");
 });
-console.log("Serdoc34ker...");
+console.log("Serdoc6634ker...");
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
